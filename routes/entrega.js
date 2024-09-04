83b5ca8e944967b0c784088b058c8fb3e54a73e1
Ronaldo <Ronaldo@email.com>
2024-09-04 09:27:29 -0300

    update e delete

--- routes/entrega.js
+++ routes/entrega.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 const entregaSchema = new mongoose.Schema({
-    IdEntrega: Number,
-    IdCarrinho: Number,
+    IdCarrinho: {type: mongoose.Schema.Types.ObjectId, ref: 'Carrinho'} ,
     Partida: String,
     Destino: String,
     DataHora: Date,
@@ -26,7 +25,6 @@ router.get('/entrega', async (req, res) => {
 router.post('/entrega', async (req, res) => {
     try {
         const newEntrega = new Entrega({
-            IdEntrega: req.body.IdEntrega,
             IdCarrinho: req.body.IdCarrinho,
             Partida: req.body.Partida,
             Destino: req.body.Destino,
@@ -40,5 +38,52 @@ router.post('/entrega', async (req, res) => {
         res.status(400).json({ error: 'Failed to create record' });
       }
 })
+
+router.patch('/entrega/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+      const updatedEntrega = await Entrega.findByIdAndUpdate(
+          id, 
+          {
+              IdCarrinho: req.body.IdCarrinho,
+              Partida: req.body.Partida,
+              Destino: req.body.Destino,
+              DataHora: req.body.DataHora,
+              Status: req.body.Status
+          }, 
+          { 
+              new: true, // Return the updated document
+              runValidators: true // Validate the update against the schema
+          }
+      );
+
+      if (!updatedEntrega) {
+          return res.status(404).json({ error: 'Entrega não encontrada' });
+      }
+
+      res.json(updatedEntrega);
+  } catch (error) {
+      res.status(400).json({ error: 'Erro ao atualizar a entrega', details: error.message });
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+      const deletedEntrega = await Entrega.findByIdAndDelete(id);
+
+      if (!deletedEntrega) {
+          return res.status(404).json({ error: 'Entrega não encontrada' });
+      }
+
+      res.json({ message: 'Entrega removida com sucesso' });
+  } catch (error) {
+      res.status(500).json({ error: 'Erro ao remover a entrega', details: error.message });
+  }
+});
+
 
 module.exports = router
